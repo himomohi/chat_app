db77ad22ad48ff2ac9f19df72e0aaaea453d8076
appcaster <himomohi@gmail.com>
2022-03-18 17:17:20 +0900

    roomChat update

--- js/chat.js
+++ js/chat.js
@@ -1,25 +1,74 @@
 
-const eventSource = new EventSource("http://localhost:8080/sender/ssar/receiver/cos");
+//록인 시스템 대신 임시방편
+let username = prompt("닉네임을 설정하세요");
+let roomNum = prompt("방번호를 입력하세요");
+
+document.querySelector("#username").innerHTML = username;
+//SSE 연결하기
+const eventSource = new EventSource('http://localhost:8080/chat/roomNum/' + roomNum);
 
 
 eventSource.onmessage = (e) => {
-    console.log(1, e);
+
     const data = JSON.parse(e.data);
-    console.log(2, e);
-    initMessage(data);
+    if (data.sender == username) { //로그인 한 유저가 보낸 메시지
+        //파란박스 (오른쪽)
+        initMyMessage(data);
+    } else {
+        //회색박스 (왼쪽)
+        initYourMessage(data);
+    }
+
+
+
+}
+function getSendMsgBox(data) {
+    let md = data.createdAt.substring(5, 10);
+    let tm = data.createdAt.substring(11, 16);
+    converTime = tm + "|" + md;
+    return '<div class="sent_msg"> <p>' + data.msg + '</p> <span class="time_date">' + converTime + "/<b>" + data.sender + ' </b></span> </div> ';
+}
+
+function getReceiveMsgBox(data) {
+    let md = data.createdAt.substring(5, 10);
+    let tm = data.createdAt.substring(11, 16);
+    converTime = tm + "|" + md;
+    return '<div class="received_withd_msg"> <p>' + data.msg + '</p> <span class="time_date">' + converTime + "/<b>" + data.sender + ' </b></span> </div> ';
+}
+
+
+
+//파란 메세지 박스 초기화
+//addMessage() 함수 호출시 DB에 insert 되고 , 그 데이터가 자동으로 흘러들어온다. (SSE 방식)
+function initMyMessage(data) {
+    //alert("클릭됨")
+
+    let chatBox = document.querySelector("#chat-box");
+
+
+
+    // alert(msgInput.value);
+
+
+    let sendBox = document.createElement("div");
+    sendBox.className = "outgoing_msg"; //채팅 css Class 추가
+    sendBox.innerHTML = getSendMsgBox(data);
+    chatBox.append(sendBox);
+
+    document.documentElement.scrollTop = document.body.scrollHeight;
+
 
 
 }
 
 
-function getSendMsgBox(msg, time) {
-    return '<div class="sent_msg"> <p>' + msg + '</p> <span class="time_date">' + time + ' </span> </div> ';
-}
 
 
 
 //메세지 박스 초기화
-function initMessage(data) {
+//회색박스
+//최초 초기화 될때 1번방 3건이 있으면 3건을 다가져온다
+function initYourMessage(data) {
     //alert("클릭됨")
 
     let chatBox = document.querySelector("#chat-box");
@@ -30,37 +79,35 @@ function initMessage(data) {
     // alert(msgInput.value);
 
 
-    let chatOutgoingBox = document.createElement("div");
-    chatOutgoingBox.className = "outgoing_msg"; //채팅 css Class 추가
-    chatOutgoingBox.innerHTML = getSendMsgBox(data.msg, data.createdAt);
+    let receivedBox = document.createElement("div");
+    receivedBox.className = "received_msg"; //채팅 css Class 추가
+    receivedBox.innerHTML = getReceiveMsgBox(data);
     console.log(data.createAt);
-    chatBox.append(chatOutgoingBox);
-    msgInput.value = "";
+    chatBox.append(receivedBox);
+    document.documentElement.scrollTop = document.body.scrollHeight;
+
+
 
 
 }
 //메세지 박스
+//AJAX 채팅 메시지 전송
 async function addMessage() {
     //alert("클릭됨")
 
-    let chatBox = document.querySelector("#chat-box");
 
     //input id를 찾아서 msgInput에 대입
     let msgInput = document.querySelector("#chat-outgoing-msg");
 
-    // alert(msgInput.value);
 
-    //시간추가
-    let date = new Date();
-    let now = date.getHours() + ":" + date.getMinutes() + "|" + date.getMonth() + "/" + date.getDate();
 
     let chat = {
-        sender: "ssar",
-        receiver: "cos",
+        sender: username,
+        roomNum: roomNum,
         msg: msgInput.value
     };
 
-    let response = await fetch("http://localhost:8080/chat",
+    fetch("http://localhost:8080/chat",
         {
             method: "post",
             body: JSON.stringify(chat), //chat 을 JSON 으로 변경
@@ -69,22 +116,7 @@ async function addMessage() {
             }
 
         });
-
-    let parseResponse = await response.json(); //전달받은 값을 json 으로 변경
-
-    console.log(parseResponse);
-
-
-
-
-    let chatOutgoingBox = document.createElement("div");
-    chatOutgoingBox.className = "outgoing_msg"; //채팅 css Class 추가
-
-    //chatOutgoingBox 에  msginput 의 값 과 , now() 를 넘겨줌
-    chatOutgoingBox.innerHTML = getSendMsgBox(msgInput.value, now);
-    chatBox.append(chatOutgoingBox);
     msgInput.value = "";
-
 
 }
 
